7508ca9ec61d62f6a64da3f978c0acd9a947a561
Rob Allen <rob@akrabat.com>
2017-05-07 12:10:10 +0100

    Update JSON & XML body parsers
    Sync with Slim 3.x code.

--- src/Request.php
+++ src/Request.php
@@ -149,22 +149,28 @@ class Request extends Message implements ServerRequestInterface
         }
 
         $this->registerMediaTypeParser('application/json', function ($input) {
-            return json_decode($input, true);
-        });
-
-        $this->registerMediaTypeParser('application/xml', function ($input) {
-            $backup = libxml_disable_entity_loader(true);
-            $result = simplexml_load_string($input);
-            libxml_disable_entity_loader($backup);
+            $result = json_decode($input, true);
+            if (!is_array($result)) {
+                return null;
+            }
             return $result;
         });
 
-        $this->registerMediaTypeParser('text/xml', function ($input) {
+
+        $xmlMediaTypeParser = function ($input) {
             $backup = libxml_disable_entity_loader(true);
+            $backup_errors = libxml_use_internal_errors(true);
             $result = simplexml_load_string($input);
             libxml_disable_entity_loader($backup);
+            libxml_clear_errors();
+            libxml_use_internal_errors($backup_errors);
+            if ($result === false) {
+                return null;
+            }
             return $result;
-        });
+        };
+        $this->registerMediaTypeParser('application/xml', $xmlMediaTypeParser);
+        $this->registerMediaTypeParser('text/xml', $xmlMediaTypeParser);
 
         $this->registerMediaTypeParser('application/x-www-form-urlencoded', function ($input) {
             parse_str($input, $data);
